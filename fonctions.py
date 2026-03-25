2b22ad80a1b9aa6fcba7515a3ef2722348d6d885
carl7k08 <jacobcarlos744@gmail.com>
2026-03-25 17:36:36 +0100

    Débogage de \creer, j'avais oublié des parenthèses donc décode n'était pas pris en compte

--- fonctions.py
+++ fonctions.py
@@ -3,5 +3,5 @@ from cryptography.fernet import Fernet
 
 def creer_groupe():
     uid = str(uuid.uuid4())
-    cle_fernet = Fernet.generate_key.decode("utf-8")
+    cle_fernet = Fernet.generate_key().decode("utf-8")
     return uid, cle_fernet
