aae91ce674430529b04697269de65c4bae004445
carl7k08 <jacobcarlos744@gmail.com>
2026-03-25 15:19:11 +0100

    Ajout du discussions.html dans le dossier templates et ajout du script fonctions.py permettant de génerer des UID uniques qui permettront d'identifier les groupes dans la base et des cle_fernet qui permettront de chiffrer et de dechiffrer les messages transmis

---
+++ fonctions.py
@@ -0,0 +1,7 @@
+import uuid
+from cryptography.fernet import Fernet
+
+def creer_groupe():
+    uid = str(uuid.uuid4())
+    cle_fernet = Fernet.generate_key.decode("utf-8")
+    return uid, cle_fernet
