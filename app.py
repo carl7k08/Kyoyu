f399a3415d5117ac5c0150e0bf08d7b30a904d40
carl7k08 <jacobcarlos744@gmail.com>
2026-03-25 16:14:15 +0100

    J'ai oublié d'ajouter le chemin du script dans discussions.html

--- app.py
+++ app.py
@@ -21,8 +21,14 @@ def accueil():
 @app.route("/creer", methods=["POST"])
 def creer():
     nouvel_uid, nouvelle_cle = creer_groupe()
+    requete_sql = "INSERT INTO groupes (uid_secret, cle_chiffrement) VALUES (%s, %s)"
 
+    valeurs = (nouvel_uid, nouvelle_cle)
 
+    cursor.execute(requete_sql, valeurs)
+    db.commit()
+
+    return f"Groupe crée ! Voici l'UID secret : {nouvel_uid} (A CONSERVER PRECIEUSEMENT ! COMME SI VOTRE VIE EN DEPENDAIT !!!!)"
 
 if __name__ == "__main__":
     app.run()
